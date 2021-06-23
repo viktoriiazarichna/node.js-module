b4f545e75d7bfa365497d26d1d0b78626cbd1f49
viktoriiazarichna <viktoriiazarichna@gmail.com>
2021-06-23 19:16:58 +0300

    homework1

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-// const switchStudents = require('./switchStudents');
-const groupByGender = require('./groupByGender');
 
-// switchStudents();
-groupByGender();
-
-
-// const fs = require ('fs');
+const fs = require ('fs');
 
 
 // fs.mkdir(`${__dirname}/dec-2020`, (err) => {
@@ -28,6 +22,121 @@ groupByGender();
 //     }
 // })
 
+
+const path = require('path')
+
+let dirBoys = path.join(__dirname, 'dec-2020', 'boys');
+let dirGirls = path.join(__dirname, 'dec-2020', 'girls');
+
+
+// console.log(dirBoys);
+// console.log(dirGirls);
+
+// fs.mkdir(`${__dirname}/dec-2020/boys`, (err) => {
+//     console.log(err);
+// });
+//
+// fs.mkdir(`${__dirname}/dec-2020/girls`, (err) => {
+//     console.log(err);
+// });
+
+
+//---Task1----
+//     fs.readdir(`${__dirname}/dec-2020/18.00`, (err, files) => {
+//         if (err) {
+//             console.log(err);
+//             return
+//         }
+//         files.forEach(file => {
+//             fs.rename(`${__dirname}/dec-2020/18.00/${file}`,  `${__dirname}/dec-2020/20.00/${file}`, err => {
+//                 if (err) {
+//                     console.log(err);
+//
+//                 }
+//             })
+//         })
+//     })
+//
+//     fs.readdir(`${__dirname}/dec-2020/20.00`, (err, files) => {
+//         if (err) {
+//             console.log(err);
+//             return
+//         }
+//         files.forEach(file => {
+//             fs.rename(`${__dirname}/dec-2020/20.00/${file}`, `${__dirname}/dec-2020/18.00/${file}`, err => {
+//                 if (err) {
+//                     console.log(err);
+//
+//                 }
+//             })
+//         })
+//     })
+
+
+//---Task2----
+// fs.readdir(`${__dirname}/dec-2020/18.00`, (err, files) => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     files.forEach(file => {
+//         fs.readFile(`${__dirname}/dec-2020/18.00/${file}`, (err1, data) => {
+//             if (err1) {
+//                 console.log(err1);
+//                 return
+//             }
+//
+//            const newFile = JSON.parse(data.toString())
+//             if(newFile.gender === 'male') {
+//                 fs.rename(`${__dirname}/dec-2020/18.00/${file}`, `${__dirname}/dec-2020/boys/${file}`, err1 => {
+//                     if (err1) {
+//                         console.log(err1);
+//                     }
+//                 })
+//             } else {
+//                 fs.rename(`${__dirname}/dec-2020/18.00/${file}`, `${__dirname}/dec-2020/girls/${file}`, err1 => {
+//                     if (err1) {
+//                         console.log(err1);
+//                     }
+//                 })
+//             }
+//
+//         })
+//
+//
+//     })
+// })
+
+
+fs.readdir(`${__dirname}/dec-2020/20.00`, (err, files) => {
+    if (err) {
+        console.log(err);
+        return
+    }
+    files.forEach(file => {
+        fs.readFile(`${__dirname}/dec-2020/20.00/${file}`, (err1, data) => {
+            if (err1) {
+                console.log(err1);
+                return
+            }
+
+            const newFile = JSON.parse(data.toString())
+            if(newFile.gender === 'male') {
+                fs.rename(`${__dirname}/dec-2020/20.00/${file}`, `${__dirname}/dec-2020/boys/${file}`, err1 => {
+                    if (err1) {
+                        console.log(err1);
+                    }
+                })
+            } else {
+                fs.rename(`${__dirname}/dec-2020/20.00/${file}`, `${__dirname}/dec-2020/girls/${file}`, err1 => {
+                    if (err1) {
+                        console.log(err1);
+                    }
+                })
+            }
+        })
+    })
+})
 
 
 
