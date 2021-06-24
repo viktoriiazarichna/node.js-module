4964bba88ec789fac5c4c7ecda22cc886a54d290
viktoriiazarichna <viktoriiazarichna@gmail.com>
2021-06-24 23:07:27 +0300

    lesson2

--- app.js
+++ app.js
@@ -3,28 +3,41 @@ const expressHbs = require('express-handlebars');
 const path = require ('path');
 
 const users = [
-    {name: 'Vika', age: 18},
-    {name: 'Ihor', age: 24},
-    {name: 'Anya', age: 15},
-    {name: 'Olya', age: 28}
+    {name: 'Vika', age: 18, password:'1234'},
+    {name: 'Ihor', age: 24, password: '1234'},
+    {name: 'Anya', age: 15, password: '1234'},
+    {name: 'Olya', age: 28, password: '1234'}
 ]
 
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', '.hbs');
 app.engine('.hbs', expressHbs({
     defaultLayout: false
 }));
+
 app.set('views', path.join(__dirname, 'static'));
 
-app.use(express.json());
+
 
 
 app.get('/users', (req, res) => {
-    res.render('users', {name: 'Dina', users} )
+    res.render('users', {name: 'Dina', users} );
 });
+
+app.get('/login', (req, res) => {
+    res.render('login', {isAdult: true});
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.json('OK');
+    const result = users.find(({name}) => name === 'Vika');
+    console.log(result);
+})
 
 // app.get('/', (req, res) => {
 //     // console.log(req);
